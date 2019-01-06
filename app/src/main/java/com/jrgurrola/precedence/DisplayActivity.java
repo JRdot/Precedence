024ded939a90cedeaf2d223ac7601be1b88dbe20
JRdot <40710331+JRdot@users.noreply.github.com>
2019-01-05 19:14:56 -0800

    Implemented onSaveInstanceState()

--- app/src/main/java/com/jrgurrola/precedence/DisplayActivity.java
+++ app/src/main/java/com/jrgurrola/precedence/DisplayActivity.java
@@ -12,28 +12,32 @@ import java.util.PriorityQueue;
 
 public class DisplayActivity extends Activity {
     public static final String LIST_OF_TASKS = "tasklist";
+    //Note to self: not "final" since arrays aren't static
+    public static ArrayList<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
-
         //Get intent that started this activity
         Intent sentIntent = getIntent();
-
-        //Get list of strings
-
-        //Note to self: In 1st activity, we put an arraylist with name "tasklist"
-        //Here, we retrieve using the same name a la final String.
-        ArrayList<String> list = sentIntent.getStringArrayListExtra(LIST_OF_TASKS);
-
-        ListView lv = (ListView) findViewById(R.id.show_list);
-
+        //If list was previously created...
+        if(savedInstanceState != null){
+            list = savedInstanceState.getStringArrayList("list");
+        } else {//List not created, get from intent
+            list = sentIntent.getStringArrayListExtra(LIST_OF_TASKS);
+        }
+        ListView lv = findViewById(R.id.show_list);
         //Create array adapter to diplay "list" onto "lv"
         ArrayAdapter<String> taskList = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_activated_1, list);
-
         lv.setAdapter(taskList);
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putStringArrayList("list", list);
 
     }
 
